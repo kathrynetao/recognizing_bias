7544901210cc4c8e987b0b0106752b5c38480fb8
kathrynetao <61030954+kathrynetao@users.noreply.github.com>
2021-10-12 14:44:06 -0500

    Add files via upload

---
+++ bias.py
@@ -0,0 +1,21 @@
+import nltk
+nltk.download()
+from nltk.stem import PorterStemmer
+from nltk.stem import LancasterStemmer
+
+bias_bank = {
+    "Emerge": 0, "Serious": 0, "Refuse": 0, "Crucial": 0, "High-stakes": 0, "Tirade": 0, "Landmark": 0, "Latest in a string of": 0, "Major": 0, "Turn up the heat": 0, "Critical": 0, "Decrying": 0, "Offend": 0, "Stern talks": 0, "Offensive": 0, "Facing calls to": 0, "Meaningful": 0, "Even though": 0, "Monumental": 0, "Significant": 0,
+    "Finally": 0, "Surfaced": 0, "Acknowledged": 0, "Emerged": 0, "Refusing to say": 0, "Conceded": 0, "Dodged": 0, "Admission": 0, "Came to light": 0, "Admit to": 0,
+    "Mocked": 0, "Raged": 0, "Bragged": 0 , "Fumed": 0, "Lashed out": 0, "Incensed": 0, "Scoffed": 0, "Frustration": 0, "Erupted": 0, "Rant": 0, "Boasted": 0, "Gloated": 0,
+    "Good": 0, "Better": 0, "Best": 0, "Is considered to be": 0, "Seemingly": 0, "Extreme": 0, "May mean that": 0, "Could": 0, "Apparently": 0, "Bad": 0, "Worse": 0, "Worst": 0, "It's likely that": 0, "Dangerous": 0, "Suggests": 0, "Would seem": 0, "Decrying": 0, "Possibly": 0,
+    "Shocking": 0, "Remarkable": 0, "Rips": 0, "Chaotic": 0, "Lashed out": 0, "Onslaught": 0, "Scathing": 0, "Showdown": 0, "Explosive": 0, "Slams": 0, "Forcing": 0, "Warning": 0, "Embroiled in": 0, "Torrent of tweets": 0, "Desperate": 0
+}
+
+def sentiment_analysis(str):
+
+def most_frequent(str):
+
+def bias_word_count(str):
+
+
+def part_of_speech(str):
