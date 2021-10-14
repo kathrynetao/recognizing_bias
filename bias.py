5d60c15c4fc0c93e42811cf4cbcb21fc421b723d
Kathryne Tao <kathryne@dhcp-10-105-48-8.wireless.northwestern.private>
2021-10-14 15:12:34 -0500

    more specified terms

--- bias.py
+++ bias.py
@@ -21,7 +21,12 @@ print(data)
 #creating the stemmer
 snow = SnowballStemmer(language='english')
 
-bias_bank = {
+bias_word_lst = ["Emerge", "Serious", "Refuse", "Crucial", "High-stakes", "Tirade", "Landmark", "Latest in a string of", "Major", "Turn up the heat", "Critical", "Decrying", "Offend", "Stern talks", "Offensive", "Facing calls to", "Meaningful", "Even though", "Monumental", "Significant",
+"Finally", "Surfaced", "Acknowledged", "Emerged", "Refusing to say", "Conceded", "Dodged", "Admission", "Came to light", "Admit to", "Mocked", "Raged", "Bragged", "Fumed", "Lashed out", "Incensed", "Scoffed", "Frustration", "Erupted", "Rant", "Boasted", "Gloated",
+"Good", "Better", "Best", "Is considered to be", "Seemingly", "Extreme", "May mean that", "Could", "Apparently", "Bad", "Worse", "Worst", "It's likely that", "Dangerous", "Suggests", "Would seem", "Decrying", "Possibly",
+"Shocking", "Remarkable", "Rips", "Chaotic", "Lashed out", "Onslaught", "Scathing", "Showdown", "Explosive", "Slams", "Forcing", "Warning", "Embroiled in", "Torrent of tweets", "Desperate"]
+
+stemmed_bias_words = {
     "emerg": 0, "serious": 0, "refus": 0, "crucial": 0, "high-stak": 0, "tirad": 0, "landmark": 0, "latest in a string of": 0, "major": 0, "turn up the heat": 0, "critic": 0, "decri": 0, "offens": 0, "stern talk": 0, "facing calls to": 0, "meaning": 0, "even though": 0, "monument": 0, "signific": 0,
     "final": 0, "surfac": 0, "acknowledg": 0, "emerg": 0, "refusing to say": 0, "conced": 0, "dodg": 0, "admiss": 0, "came to light": 0, "admit to": 0,
     "mock": 0, "rage": 0, "brag": 0 , "fume": 0, "lashed out": 0, "incens": 0, "scof": 0, "frustrat": 0, "erupt": 0, "rant": 0, "boast": 0, "gloat": 0,
@@ -29,17 +34,12 @@ bias_bank = {
     "shock": 0, "remark": 0, "rip": 0, "chaotic": 0, "lashed out": 0, "onslaught": 0, "scath": 0, "showdown": 0, "explos": 0, "slam": 0, "forc": 0, "warn": 0, "embroiled in": 0, "torrent of tweet": 0, "desper": 0
 }
 
-bias_word_lst = ["Emerge", "Serious", "Refuse", "Crucial", "High-stakes", "Tirade", "Landmark", "Latest in a string of", "Major", "Turn up the heat", "Critical", "Decrying", "Offend", "Stern talks", "Offensive", "Facing calls to", "Meaningful", "Even though", "Monumental", "Significant",
-"Finally", "Surfaced", "Acknowledged", "Emerged", "Refusing to say", "Conceded", "Dodged", "Admission", "Came to light", "Admit to", "Mocked", "Raged", "Bragged", "Fumed", "Lashed out", "Incensed", "Scoffed", "Frustration", "Erupted", "Rant", "Boasted", "Gloated",
-"Good", "Better", "Best", "Is considered to be", "Seemingly", "Extreme", "May mean that", "Could", "Apparently", "Bad", "Worse", "Worst", "It's likely that", "Dangerous", "Suggests", "Would seem", "Decrying", "Possibly",
-"Shocking", "Remarkable", "Rips", "Chaotic", "Lashed out", "Onslaught", "Scathing", "Showdown", "Explosive", "Slams", "Forcing", "Warning", "Embroiled in", "Torrent of tweets", "Desperate"]
-
 
 def sentiment_analysis(str):
     testimonial = TextBlob(str)
     return testimonial.sentiment
 
-sentiment_analysis(data)
+# sentiment_analysis(data)
 #
 # # def most_frequent(str):
 #
@@ -51,7 +51,7 @@ def __strip(str):
 
 def bias_word_count(str):
     counter = 0
-    bias_lst = []
+    bias_word_count = []
     str = __strip(str)
     token_words = word_tokenize(str)
     stem_sentence = []
@@ -59,14 +59,14 @@ def bias_word_count(str):
         stem_sentence.append(snow.stem(word))
 
     for word in stem_sentence:
-        if word in bias_bank:
-            bias_bank[word] += 1
+        if word in stemmed_bias_words:
+            stemmed_bias_words[word] += 1
             counter += 1
-            bias_lst.append(word)
-    return [counter, bias_lst]
+            bias_word_count.append(word)
+    return [counter, bias_word_count]
 
 
-bias_word_count(data)
+# bias_word_count(data)
 
 def main(str):
     subj = sentiment_analysis(str).subjectivity
