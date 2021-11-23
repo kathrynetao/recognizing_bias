6b3810d6ac0a21b99d1a411b65fd08b0422f8b1f
Zheng Kaixin <574998054@qq.com>
2021-11-23 14:32:00 -0600

    change back to old one

--- demo.py
+++ demo.py
@@ -10,30 +10,44 @@ def index():
   if request.method == 'GET':
     return render_template('index.html', input_text = '', res_text = '')
   else:
-    inputText = request.form.get("input_text")
-    # return render_template('index.html', input_text = inputText)
-    inputText = urllib.parse.quote_plus(inputText)
+        inputText = request.form.get("input_text")
+        html = bias.get_html(inputText)
+        base_url = 'https://' + bias.base_url(inputText)
+        html = html.decode("utf-8")
+        if 'href="/' in html:
+            html = html.replace('href="/', 'href="' + base_url + '/')
 
-    return redirect(url_for('news',restext = inputText))
+        data= bias.get_article(inputText)
+        found_reg_list, index_list = ne.number_extract(data)
+        count = 0
+        for i in index_list:
+          data = data[0: i[0] + count * 31] + '<span class = "numbers">' + data[i[0] + count * 31:i[1]+ count * 31] + '</span>' + data[i[1]+ count * 31:]
+          count += 1
 
-@app.route('/news/<restext>')
-def news(restext):
-    restext = urllib.parse.unquote(restext)
-    html = bias.get_html(restext)
-    base_url = 'https://' + bias.base_url(restext)
-    html = html.decode("utf-8")
-    if 'href="/' in html:
-        html = html.replace('href="/', 'href="' + base_url + '/')
+        resText = Markup(html)
 
-    data= bias.get_article(restext)
-    found_reg_list, index_list = ne.number_extract(data)
-    count = 0
-    for i in index_list:
-      data = data[0: i[0] + count * 31] + '<span class = "numbers">' + data[i[0] + count * 31:i[1]+ count * 31] + '</span>' + data[i[1]+ count * 31:]
-      count += 1
+        return render_template('index.html', input_text = inputText,res_text = resText)
+    # inputText = urllib.parse.quote_plus(inputText)
+    # return redirect(url_for('news',restext = inputText))
 
-    resText = Markup(html)
-    return render_template('news.html',res_text = resText)
+# @app.route('/news/<restext>')
+# def news(restext):
+#     restext = urllib.parse.unquote(restext)
+#     html = bias.get_html(restext)
+#     base_url = 'https://' + bias.base_url(restext)
+#     html = html.decode("utf-8")
+#     if 'href="/' in html:
+#         html = html.replace('href="/', 'href="' + base_url + '/')
+#
+#     data= bias.get_article(restext)
+#     found_reg_list, index_list = ne.number_extract(data)
+#     count = 0
+#     for i in index_list:
+#       data = data[0: i[0] + count * 31] + '<span class = "numbers">' + data[i[0] + count * 31:i[1]+ count * 31] + '</span>' + data[i[1]+ count * 31:]
+#       count += 1
+#
+#     resText = Markup(html)
+#     return render_template('news.html',res_text = resText)
 
 if __name__ == '__main__':
     app.run()
